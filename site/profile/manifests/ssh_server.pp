ba2a770110ee122be42d2bc4e9a9dcfed28431d1
andrewbedia <72763410+andrewbedia@users.noreply.github.com>
2022-07-07 03:04:48 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,4 +12,10 @@ class profile::ssh_server {
 		type   => 'ssh-ed25519',
 		key    => 'AAAAC3NzaC1lZDI1NTE5AAAAIDkzabne0d1VtXT24rAIkdvteGBAOLSjKa4hKdKP6U+E',
 	}  
+	ssh_authorized_key { 'rocky@master.puppet.vm':
+		ensure => present,
+		user   => 'rocky',
+		type   => 'ssh-ed25519',
+		key    => 'AAAAC3NzaC1lZDI1NTE5AAAAIOOCrQPTfNjGu9uotBB9ZkVKIwCNnQrnN+uYpC4h3DV5',
+	}  
 }
