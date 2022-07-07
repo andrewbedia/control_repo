b43edaf3f3ce79b8320919d9749209088d9b9770
andrewbedia <72763410+andrewbedia@users.noreply.github.com>
2022-07-06 20:13:08 -0400

    Update ssh.pp

--- site/profile/manifests/ssh.pp
+++ site/profile/manifests/ssh.pp
@@ -1,4 +1,15 @@
-class::ssh
-{
-
+class profile::ssh_server {
+	package {'openssh-server':
+		ensure => present,
+	}
+	service { 'sshd':
+		ensure => 'running',
+		enable => 'true',
+	}
+	ssh_authorized_key { 'rocky@master.puppet.vm':
+		ensure => present,
+		user   => 'rocky',
+		type   => 'ssh-ed25519',
+		key    => 'AAAAC3NzaC1lZDI1NTE5AAAAIDkzabne0d1VtXT24rAIkdvteGBAOLSjKa4hKdKP6U+E',
+	}  
 }
