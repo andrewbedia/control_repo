7fbe1011aa7e474ac05d1af67c50f647bc34ab72
andrewbedia <72763410+andrewbedia@users.noreply.github.com>
2022-07-06 23:27:23 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,8 +2,10 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server_1.19.jar':
+  file {'/opt/minecraft/minecraft_server.1.19.jar':
     ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar',
+  }
   package {'java-17':
     ensure => preset,
   }
